b3afc754f4c611cb2140b65ad64ac933a8398a80
U-ADPROD\vipatil <vipatil@WIN-VIPATIL-R1.adprod.bmc.com>
2016-10-11 15:48:02 +0530

    updated

--- module1_solution/scripts/app.js
+++ module1_solution/scripts/app.js
@@ -9,25 +9,28 @@
 
   .controller('LunchCheckController', LunchCheckController);
 
+  /**
+   * [$inject description]
+   * @type {Array}
+   */
   LunchCheckController.$inject = ['$scope', 'growl']
   function LunchCheckController($scope, growl){
 
     $scope.lunchMenu = "";
-    $scope.message = "";
-
+    /**
+     * [checkMenu description]
+     * @return {[type]} [description]
+     */
     $scope.checkMenu = function() {
-
       var config = {};
       var menuStr = $scope.lunchMenu.trim();
 
       if (menuStr.length == 0) {
-        //$scope.message = "Please enter some data";
         growl.error("Please enter some data");
         return;
       }
       var tokenList = menuStr.split(",");
       var count = 0;
-
       for (var i in tokenList) {
         if(tokenList[i].trim().length > 0) {
           count++;
@@ -35,14 +38,10 @@
       }
 
       if (count <= 3) {
-          //$scope.message = "Enjoy!";
           growl.success("Enjoy!!");
       } else {
           growl.warning("Too Much!");
-        //$scope.message = "Too Much!";
       }
     };
   };
-
-
 })();
